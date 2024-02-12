977af3a3901face5656e9a13c8c1c3136de0565c
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-12 11:12:03 +0100

    Adding ExperimentalAttribute

---
+++ Csharp version 12/ExperimentalAttribute/ExperimentalAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DotNet8Examples
+{
+    [Experimental(diagnosticId: "Test001")]
+    public class ExperimentalAttributeDemo
+    {
+        //[Experimental(diagnosticId: "Test002")]
+        public void Print()
+        {
+            Console.WriteLine("Hello Experimental Attribute");
+        }
+    }
+}
---
+++ Csharp version 12/ExperimentalAttribute/Program.cs
@@ -0,0 +1,12 @@
+﻿using DotNet8Examples;
+
+Console.WriteLine($"Experimental Attribute examples:{Environment.NewLine}");
+
+#pragma warning disable Test001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var demo1 = new ExperimentalAttributeDemo();
+#pragma warning restore Test001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+// It's configured on .csproj to ignore the Test002;
+demo1.Print();
+
+Console.WriteLine("-------------------------");
