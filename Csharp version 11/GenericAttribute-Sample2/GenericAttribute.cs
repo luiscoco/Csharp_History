a477cc76b9c1aad7218b2c77b3b036967236fdea
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-12 13:27:59 +0100

    Adding Generic Attribute sample 2

---
+++ Csharp version 11/GenericAttribute-Sample2/GenericAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAttribute_Sample2
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ConfigAttribute<T> : Attribute
+    {
+        public string Key { get; }
+
+        public ConfigAttribute(string key)
+        {
+            Key = key;
+        }
+    }
+
+    class ApplicationSettings
+    {
+        [Config<string>("App:Name")]
+        public string Name { get; set; }
+
+        [Config<int>("App:MaxUsers")]
+        public int MaxUsers { get; set; }
+
+        [Config<bool>("App:FeatureEnabled")]
+        public bool FeatureEnabled { get; set; }
+    }
+
+}
---
+++ Csharp version 11/GenericAttribute-Sample2/Program.cs
@@ -0,0 +1,39 @@
+﻿using GenericAttribute_Sample2;
+
+var settings = new ApplicationSettings();
+LoadConfiguration(settings);
+
+Console.WriteLine($"Name: {settings.Name}");
+Console.WriteLine($"MaxUsers: {settings.MaxUsers}");
+Console.WriteLine($"FeatureEnabled: {settings.FeatureEnabled}");
+
+static void LoadConfiguration(object configObject)
+{
+    // Simulated configuration source
+    var configuration = new Dictionary<string, object>
+    {
+        { "App:Name", "My Application" },
+        { "App:MaxUsers", 100 },
+        { "App:FeatureEnabled", true }
+    };
+
+    // Iterate over all properties of the object
+    foreach (var prop in configObject.GetType().GetProperties())
+    {
+        // Check if the property has a ConfigAttribute, regardless of its generic type
+        var attr = prop.GetCustomAttributes(typeof(ConfigAttribute<>), false);
+        if (attr.Length > 0)
+        {
+            var configAttr = attr[0]; // Assuming only one ConfigAttribute per property
+            var keyProperty = configAttr.GetType().GetProperty("Key");
+            var key = keyProperty.GetValue(configAttr) as string;
+
+            // If the key exists in the configuration, set the property value
+            if (key != null && configuration.ContainsKey(key))
+            {
+                var value = Convert.ChangeType(configuration[key], prop.PropertyType);
+                prop.SetValue(configObject, value);
+            }
+        }
+    }
+}
