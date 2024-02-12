466c9760e3d3e7fc57408852472175b4e4823f61
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-12 09:34:16 +0100

    Adding InlineArray

---
+++ Csharp version 12/InlineArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿InlineArrayDemo inlineArray = new InlineArrayDemo();
+
+for (int i = 0; i < 10; i++)
+{
+    inlineArray[i] = i;
+}
+
+foreach (var item in inlineArray)
+{
+    Console.Write($"{item} ");
+}
+
+// Output:
+//0 1 2 3 4 5 6 7 8 9
+
+[System.Runtime.CompilerServices.InlineArray(10)]
+public struct InlineArrayDemo
+{
+    private int _element;
+}
