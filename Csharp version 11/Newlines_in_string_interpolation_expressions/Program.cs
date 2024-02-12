c0b848723ca35ccde3548b1446ef16fcb2c9e431
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-12 13:42:38 +0100

    Adding Newlines in string interpolation expressions

---
+++ Csharp version 11/Newlines_in_string_interpolation_expressions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NewlineInterpolationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John";
+            string lastName = "Doe";
+
+            // Using newlines directly in string interpolation
+            string greeting = $"""
+                Hello, 
+                {firstName} {lastName}!
+                Welcome to C# 11.
+                """;
+
+            Console.WriteLine(greeting);
+        }
+    }
+}
