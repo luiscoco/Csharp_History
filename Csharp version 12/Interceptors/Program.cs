67172f5e7fd551294be2009aaa66c0f6a3910ba2
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-12 10:56:52 +0100

    Adding Interceptors

---
+++ Csharp version 12/Interceptors/Program.cs
@@ -0,0 +1,11 @@
+﻿using InterceptorsTest;
+
+Console.WriteLine($"Interceptors example:{Environment.NewLine}");
+
+var interceptableExample = new InterceptableExample();
+
+interceptableExample.PrintValue1();
+interceptableExample.PrintValue2(); // The method PrintValue2 will be intercepted;
+interceptableExample.PrintValue3();
+
+Console.WriteLine("-------------------------");
