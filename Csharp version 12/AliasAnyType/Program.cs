57bba24054bf9328c9b605436866c5e558462c9c
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-12 10:11:45 +0100

    Adding AliasAnyType

---
+++ Csharp version 12/AliasAnyType/Program.cs
@@ -0,0 +1,40 @@
+﻿using DotNet8;
+using System;
+using Person = (string name, int age, string country);
+using MyConsole = System.Console;
+
+#region Alias any type
+
+Console.WriteLine($"Alias any type examples:{Environment.NewLine}");
+
+MyConsole.WriteLine("Test console");
+
+var person = AliasAnyType.GetPerson();
+Console.WriteLine(person);
+
+AliasAnyType.PrintPerson(person);
+
+Console.WriteLine("-------------------------");
+
+#endregion
+
+
+namespace DotNet8
+{
+    public static class AliasAnyType
+    {
+        public static Person GetPerson()
+        {
+            Person person = new("Aragorn", 33, "Netherlands");
+
+            return person;
+        }
+
+        public static void PrintPerson(Person person)
+        {
+            Console.WriteLine($"{person.name}, {person.age}, {person.country}");
+        }
+    }
+}
+
+
