90be0547a54dd490538d0f705e92ea1fe30c7f42
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-12 11:44:54 +0100

    adding code for OptionalParametersInLambdaExpressions

---
+++ Csharp version 12/OptionalParametersInLambdaExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using DotNet8Examples;
+
+Console.WriteLine($"Default Lambda Parameters examples:{Environment.NewLine}");
+
+var defaultLambdaParameters = new DefaultLambdaParameters();
+defaultLambdaParameters.Demo();
+
+Console.WriteLine("-------------------------");
+
+var addWithDefault1 = (int addTo = 2) => Console.WriteLine(addTo + 1);
+addWithDefault1();
+
+var lambdaDemo1 = (string name = "UserLuis") => Console.WriteLine($"Hello {name}");
+
+lambdaDemo1();
+
